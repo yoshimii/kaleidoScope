25915aed36609d2ae18aaa1fad67818ec0f10db0
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-02 23:13:10 -0500

    made first shape, square. You are what you make, I guess

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const view = document.getElementById("canvas");
-const context = view.getContext("2d");
+const context = view.getContext("2d");//Retrieves the 2d rendering context
 const shapes = [];
-const size = 2;
-const gridHidth = 30;
-const gridHeight = 30;
+const size = 2;//appears to set size in pixels
+const gridHidth = 30;//sets pieces per row
+const gridHeight = 30;//sets pieces per column
 const target = {x: 0, y: 0};//an object with a coordinate
 const startPos = {x: 0, y: 0};//an object with a coordinate
 const colors =["#8bf0ba", "#0e0fed", "#f2b1d8", "magenta", "blue"];
 const offset = 50;//Action speed
 let updateCount = 0;
 
-//Let's Get Into Shapes!
+//Let's Get Into Shapes!//This is where I stop to read MDN
+
+function draw() {
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+  
+      ctx.fillRect(25, 25, 100, 100);
+      ctx.clearRect(45, 45, 60, 60);
+      ctx.strokeRect(50, 50, 50, 50);
+    }
+  }
+
+draw();
 
